0855c07f3e5b9b7a6803ce2acfc58f99a36a5dbe
six-two <0x62@pm.me>
2018-11-16 13:16:36 +0100

    Made it compile, tested everything except entropy
    Put "Histogram.printHistogram(ip);" into run to test it yourself

--- src/main/java/com/bv_gruppe_d/imagej/Histogram.java
+++ src/main/java/com/bv_gruppe_d/imagej/Histogram.java
@@ -1,128 +1,169 @@
+package com.bv_gruppe_d.imagej;
+
+import ij.process.ImageProcessor;
 
 public class Histogram {
-  private final int[] count, cumulative;
-  private int min, max;
-  private final double[] moments;
-  private final double entropy;
-  private int sum;
+	private final int[] count, cumulative;
+	private int min, max;
+	private final double[] moments;
+	private final double entropy;
+	private int sum, mode;
+	
+	public static void printHistogram(ImageProcessor ip) {
+		System.out.println("Warning: I could not test entropy (didnt find a way for IJ to calculate it)");
+		System.out.println("But the rest had the same results as stuff");
+		
+		Histogram h = Histogram.calculate(ip);
 
-  public static Histogram calculate(ImageProcessor ip){
-    int[] values = new int[256];
-    int w = ip.getWidth(), h = ip.getHeight();
-    for (int x = 0; x < w; ++x){
-      for (int y = 0, y < h; ++y){
-        int pxl = ip.getPixel(x, y);
-        values[pxl] += 1;
-      }
-    }
-    return new Histogram(values);
-  }
+		double normalizedSum = 0.0;
+		for (int i = h.getMin(); i <= h.getMax(); ++i) {
+			normalizedSum = +h.getNormalizedCount(i);
+		}
+		System.out.println("Normalized sum: " + normalizedSum);
 
-  private Histogram(int[] counts){
-    this.count = counts;
-    cumulative = new int[count.length];
-    for (int i = 0; i < cumulative.length; ++i){
-      cumulative[i] = count[i];
-      if (i > 0){
-        cumulative[i] += cumulative[i - 1];
-      }
-    }
+		StringBuilder b = new StringBuilder("Min=" + h.getMin() + "|");
+		for (int i = h.getMin(); i <= h.getMax(); ++i) {
+			normalizedSum = +h.getNormalizedCount(i);
+			b.append(h.getCount(i)).append("|");
+		}
+		b.append("Max=").append(h.getMax());
+		System.out.println(b.toString());
 
-    sum = 0;
-    for (int f : count){
-      sum += f;
-      if (f < 0){
-        throw new RuntimeException("Negative number in histogram: "+f);
-      }
-    }
+		for (int i = 1; i < 5; ++i) {
+			System.out.println("Moment " + i + ": " + h.getMoment(i));
+		}
+		System.out.println("StdDev: " + Math.sqrt(h.getMoment(2)));
+		System.out.println("Mode: " + h.getMode());
+		System.out.println("Count: " + h.getSum());
+		System.out.println("Entropy: " + h.getEntropy());
+	}
 
-    min = findFirstPositive(0, count.length, 1);//forward
-    max = findFirstPositive(count.length-1, -1, -1);//backwards
+	public static Histogram calculate(ImageProcessor ip) {
+		int[] values = new int[256];
+		int w = ip.getWidth(), h = ip.getHeight();
+		for (int x = 0; x < w; ++x) {
+			for (int y = 0; y < h; ++y) {
+				int pxl = ip.getPixel(x, y);
+				values[pxl] += 1;
+			}
+		}
+		return new Histogram(values);
+	}
 
-    //Hable die formeln im buch nicht gesehen (suche nach moment)
-    moments = new int[4];
+	private Histogram(int[] counts) {
+		this.count = counts;
+		mode = 0;
+		cumulative = new int[count.length];
+		for (int i = 0; i < cumulative.length; ++i) {
+			cumulative[i] = count[i];
+			if (i > 0) {
+				cumulative[i] += cumulative[i - 1];
+			}
+			if (count[i] > count[mode]) {
+				mode = i;
+			}
+		}
 
-    //mean
-    double tmp = 0;
-    for (int i = 0; i < count.length; ++i){
-      tmp += i * count[i];
-    }
-    moments[0] = tmp / sum;
+		sum = 0;
+		for (int f : count) {
+			sum += f;
+			if (f < 0) {
+				throw new RuntimeException("Negative number in histogram: " + f);
+			}
+		}
 
-    // variance
-    tmp = 0;
-    for (int i = 0; i < count.length; ++i){
-      tmp += Math.pow(i - moments[0], 2) * count[i];
-    }
-    moments[1] = tmp / sum;
-    double stdDev = Math.sqrt(moments[1]);
+		min = findFirstPositive(0, count.length, 1);// forward
+		max = findFirstPositive(count.length - 1, -1, -1);// backwards
 
-    //skewness
-    tmp = 0;
-    for (int i = 0; i < count.length; ++i){
-      tmp += Math.pow(i - moments[0], 3) * count[i];
-    }
-    moments[2] = tmp / (sum * Math.pow(stdDev, 3));
+		// Habe die formeln im buch nicht gesehen (suche nach moment)
+		moments = new double[4];
 
-    //kurtosis
-    tmp = 0;
-    for (int i = 0; i < count.length; ++i){
-      tmp += Math.pow(i - moments[0], 4) * count[i];
-    }
-    moments[3] = (tmp / (sum * Math.pow(stdDev, 4))) - 3;
+		// mean
+		double tmp = 0;
+		for (int i = 0; i < count.length; ++i) {
+			tmp += i * count[i];
+		}
+		moments[0] = tmp / sum;
 
-    // entropy
-    tmp = 0;
-    double log2 = Math.log(2.0);
-    for (int i = 0; i < count.length; ++i){
-      double cn = getNormalizedCount(i);
-      tmp += cn * (Math.log(cn) / log2);
-    }
-    entropy = -tmp;
-  }
+		// variance
+		tmp = 0;
+		for (int i = 0; i < count.length; ++i) {
+			tmp += Math.pow(i - moments[0], 2) * count[i];
+		}
+		moments[1] = tmp / sum;
+		double stdDev = Math.sqrt(moments[1]);
 
-  private int findFirstPositive(int startIncl, int endExcl, int step){
-    int tmp = -1;
-    for (int i = start; i != end; i += step){
-      if (count[i] > 0){
-        return i;
-      }
-    }
-    throw new RuntimeException("Image has 0 pixel")
-  }
+		// skewness
+		tmp = 0;
+		for (int i = 0; i < count.length; ++i) {
+			tmp += Math.pow(i - moments[0], 3) * count[i];
+		}
+		moments[2] = tmp / (sum * Math.pow(stdDev, 3));
 
-  public int getMin(){
-    return min;
-  }
+		// kurtosis
+		tmp = 0;
+		for (int i = 0; i < count.length; ++i) {
+			tmp += Math.pow(i - moments[0], 4) * count[i];
+		}
+		moments[3] = (tmp / (sum * Math.pow(stdDev, 4))) - 3;
 
-  public int getMax(){
-    return max;
-  }
+		// entropy//TODO
+		tmp = 0;
+		double log2 = Math.log(2.0);
+		for (int i = 0; i < count.length; ++i) {
+			double cn = getNormalizedCount(i);
+			if (cn != 0) {
+				tmp += cn * (Math.log(cn) / log2);
+			}
+		}
+		entropy = -tmp;
+	}
 
-  public int getCount(int i){
-    return count[i];
-  }
+	private int findFirstPositive(int startIncl, int endExcl, int step) {
+		for (int i = startIncl; i != endExcl; i += step) {
+			if (count[i] > 0) {
+				return i;
+			}
+		}
+		throw new RuntimeException("Image has 0 pixel");
+	}
 
-  public double getNormalizedCount(i){
-    return count[i] / ((double)sum);
-  }
+	public int getMin() {
+		return min;
+	}
 
-  public int getCumulativeCount(int i){
-    return cumulative[i];
-  }
+	public int getMax() {
+		return max;
+	}
 
-  public int getSum(){
-    return sum;
-  }
+	public int getMode() {
+		return mode;
+	}
 
-  public double getMoment(int order){
-    if (order < 1 || order > 4){
-      throw new RuntimeException("Order needs to be 1, 2, 3, or 4");
-    }
-    return moments[1 + order];
-  }
+	public int getCount(int i) {
+		return count[i];
+	}
 
-  public double getEntropy(){
-    return entropy;
-  }
+	public double getNormalizedCount(int i) {
+		return count[i] / ((double) sum);
+	}
+
+	public int getCumulativeCount(int i) {
+		return cumulative[i];
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public double getMoment(int order) {
+		if (order < 1 || order > 4) {
+			throw new RuntimeException("Order needs to be 1, 2, 3, or 4");
+		}
+		return moments[order - 1];
+	}
+
+	public double getEntropy() {
+		return entropy;
+	}
 }
