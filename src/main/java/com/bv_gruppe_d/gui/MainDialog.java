cc09306e68c28dc2ec09733dde860eda4529900f
MSandeman <msandermann96@gmail.com>
2018-11-18 23:44:22 +0100

    Refactore MainDialog class.
    Split up methods.

--- src/main/java/com/bv_gruppe_d/gui/MainDialog.java
+++ src/main/java/com/bv_gruppe_d/gui/MainDialog.java
@@ -29,53 +29,70 @@ public class MainDialog extends UserDialog {
 		return topLevelPanel;
 	}
 
-	private Panel createNoicePanel() {
-		Panel noicePanel = new Panel();
-		noicePanel.setLayout(new GridLayout(2, 1));
-		
-		Label noiceHeader = new Label("Rauschen hinzufügen:");
-		noiceHeader.setAlignment(Label.CENTER);
-		noicePanel.add(noiceHeader);
-		
-		Panel noiceButtons = new Panel();	
-		JButton saltAndPepperNoiceButton = new JButton("Salt-And-Pepper Rauschen");
-		JButton gausschenNoiceButton = new JButton("Gaussches Rauschen");
-		noiceButtons.add(gausschenNoiceButton);
-		noiceButtons.add(saltAndPepperNoiceButton);
-		
-		noicePanel.add(noiceButtons);
-		return noicePanel;
-	}
-
-	private Panel createStatisticsPanel() {
-		Panel statisticsPanel = new Panel();
-		statisticsPanel.setLayout(new GridLayout(2,1));
-		
-		Label statisticsHeader = new Label("Statistik generieren:");
-		statisticsHeader.setAlignment(Label.CENTER);
-		statisticsPanel.add(statisticsHeader);
-		
-		JButton histogramCalculation = new JButton("Berechne Histogramm");
-		
-		statisticsPanel.add(histogramCalculation);
-		return statisticsPanel;
-	}
-
 	private Panel createConversionPanel() {
 		Panel conversionPanel = new Panel();
 		conversionPanel.setLayout(new GridLayout(2,1));
 		
-		Label conversionHeader = new Label("Zu Grauwertbild konvertieren:");
-		conversionHeader.setAlignment(Label.CENTER);
-		conversionPanel.add(conversionHeader);
+		conversionPanel.add(createConversionHeader());
+		conversionPanel.add(createConversionButtons());
 		
+		return conversionPanel;
+	}
+
+	private Panel createConversionButtons() {
 		Panel conversionButtons = new Panel();		
 		JButton intencityBasedTransformation = new JButton("Intensitätsbasierte Konvertierung");
 		JButton commonTransformation = new JButton("Übliche Konvertierung");
 		conversionButtons.add(intencityBasedTransformation);
 		conversionButtons.add(commonTransformation);
+		return conversionButtons;
+	}
+
+	private Label createConversionHeader() {
+		Label conversionHeader = new Label("Zu Grauwertbild konvertieren:");
+		conversionHeader.setAlignment(Label.CENTER);
+		return conversionHeader;
+	}
+	
+	private Panel createStatisticsPanel() {
+		Panel statisticsPanel = new Panel();
+		statisticsPanel.setLayout(new GridLayout(2,1));
 		
-		conversionPanel.add(conversionButtons);
-		return conversionPanel;
+		statisticsPanel.add(createStatisticsHeader());
+		JButton histogramCalculation = new JButton("Berechne Histogramm");
+		statisticsPanel.add(histogramCalculation);
+		
+		return statisticsPanel;
+	}
+
+	private Label createStatisticsHeader() {
+		Label statisticsHeader = new Label("Statistik generieren:");
+		statisticsHeader.setAlignment(Label.CENTER);
+		return statisticsHeader;
+	}	
+
+	private Panel createNoicePanel() {
+		Panel noicePanel = new Panel();
+		noicePanel.setLayout(new GridLayout(2, 1));
+		
+		noicePanel.add(createNoiceHeader());
+		noicePanel.add(createNoiceButtons());
+		
+		return noicePanel;
+	}	
+
+	private Label createNoiceHeader() {
+		Label noiceHeader = new Label("Rauschen hinzufügen:");
+		noiceHeader.setAlignment(Label.CENTER);
+		return noiceHeader;
+	}
+
+	private Panel createNoiceButtons() {
+		Panel noiceButtons = new Panel();	
+		JButton saltAndPepperNoiceButton = new JButton("Salt-And-Pepper Rauschen");
+		JButton gausschenNoiceButton = new JButton("Gaussches Rauschen");
+		noiceButtons.add(gausschenNoiceButton);
+		noiceButtons.add(saltAndPepperNoiceButton);
+		return noiceButtons;
 	}
 }
