d4a5cac9760049da2d4b5a0e5c0f0e4665e4ce2a
MSandeman <msandermann96@gmail.com>
2018-11-18 23:24:16 +0100

    Refactore to use dialog classes.

--- src/main/java/com/bv_gruppe_d/imagej/RgbToGrayscale_PlugIn.java
+++ src/main/java/com/bv_gruppe_d/imagej/RgbToGrayscale_PlugIn.java
@@ -1,6 +1,17 @@
 package com.bv_gruppe_d.imagej;
 
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import com.bv_gruppe_d.gui.MainDialog;
+
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
@@ -8,7 +19,7 @@ public class RgbToGrayscale_PlugIn implements PlugInFilter {
 	
 	@Override
 	public void run(ImageProcessor ip) {
-		
+		MainDialog dialog = new MainDialog();
 	}
 
 	@Override
