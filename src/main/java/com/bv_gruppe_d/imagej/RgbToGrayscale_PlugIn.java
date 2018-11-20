efd6e69afc33508623faed7835dffcf20fa34f00
MSandeman <msandermann96@gmail.com>
2018-11-20 10:03:30 +0100

    Handle warnings
    Remove unused imports.
    Remove unused variables.

--- src/main/java/com/bv_gruppe_d/imagej/RgbToGrayscale_PlugIn.java
+++ src/main/java/com/bv_gruppe_d/imagej/RgbToGrayscale_PlugIn.java
@@ -1,17 +1,8 @@
 package com.bv_gruppe_d.imagej;
 
-import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
 import com.bv_gruppe_d.gui.MainDialog;
 
 import ij.ImagePlus;
-import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
@@ -19,7 +10,7 @@ public class RgbToGrayscale_PlugIn implements PlugInFilter {
 	
 	@Override
 	public void run(ImageProcessor ip) {
-		MainDialog dialog = new MainDialog(ip);
+		new MainDialog(ip);
 	}
 
 	@Override
